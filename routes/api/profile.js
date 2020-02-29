9b98f896b8ecb2f6819ea94d340a3ad36969eb81
curiouslion <mehul.morker98@gmail.com>
2020-02-29 19:10:47 +0530

    post remove added in delete profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -86,22 +86,92 @@ router.post(
     if (twitter) profileFields.social.twitter = twitter;
 
     try {
-      let profile = await Profile.findOne({ user: req.user.id });
+      // let profile = await Profile.findOne({ user: req.user.id });
 
-      if (profile) {
-        // update profile
-        profile = await Profile.findOneAndUpdate(
-          { user: req.user.id },
-          { $set: profileFields },
-          { new: true }
-        );
+      // if (profile) {
+      // update profile
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true, upsert: true }
+      );
 
-        return res.json(profile);
-      }
+      res.json(profile);
+      // }
       // Create profile
 
-      profile = new Profile(profileFields);
-      await profile.save();
+      // profile = new Profile(profileFields);
+      // await profile.save();
+      // res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+/// comaparing delete one of them
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required")
+        .not()
+        .isEmpty(),
+      check("skills", "Skills is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      company,
+      location,
+      website,
+      bio,
+      skills,
+      status,
+      githubusername,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook
+    } = req.body;
+
+    const profileFields = {
+      user: req.user.id,
+      company,
+      location,
+      website: website === "" ? "" : website,
+      bio,
+      skills: Array.isArray(skills)
+        ? skills
+        : skills.split(",").map(skill => " " + skill.trim()),
+      status,
+      githubusername
+    };
+
+    // Build social object and add to profileFields
+    const socialfields = { youtube, twitter, instagram, linkedin, facebook };
+
+    for (const [key, value] of Object.entries(socialfields)) {
+      if (value.length > 0) socialfields[key] = value;
+    }
+    profileFields.social = socialfields;
+
+    try {
+      // Using upsert option (creates new doc if no match is found):
+      let profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true, upsert: true }
+      );
       res.json(profile);
     } catch (err) {
       console.error(err.message);
@@ -150,8 +220,8 @@ router.get("/user/:user_id", async (req, res) => {
 
 router.delete("/", auth, async (req, res) => {
   try {
-    // @todo - remove users post
-
+    // remove users post
+    await Post.deleteMany({ user: req.user.id });
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
 
