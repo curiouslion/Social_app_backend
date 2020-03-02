a0f6df844d971e0af5f70a67e041e9ed3476d102
curiouslion <mehul.morker98@gmail.com>
2020-03-02 12:34:24 +0530

    prepare for deployment

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const users = require("./routes/api/users");
 const auth = require("./routes/api/auth");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
+const path = require("path");
 
 // app.use(express.json());
 app.use(express.json({ extended: false }));
@@ -16,6 +17,16 @@ app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
