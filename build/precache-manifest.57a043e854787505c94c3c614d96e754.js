246ab626b620a934e541d01a7998993f7818ed4b
curiouslion <mehul.morker98@gmail.com>
2020-03-02 12:11:09 +0530

    client build added

---
+++ build/precache-manifest.57a043e854787505c94c3c614d96e754.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b99dee90be1ed5b624566760e8a37e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0819f81026f243bd4568",
+    "url": "/static/css/main.11b62bc3.chunk.css"
+  },
+  {
+    "revision": "34425eb05f8b2e6df4c7",
+    "url": "/static/js/2.e704acf8.chunk.js"
+  },
+  {
+    "revision": "2a4e0fe469a0c80fdda7591cd5b0c779",
+    "url": "/static/js/2.e704acf8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0819f81026f243bd4568",
+    "url": "/static/js/main.b52e98bf.chunk.js"
+  },
+  {
+    "revision": "941fdaaab3fc63701bef",
+    "url": "/static/js/runtime-main.76fa1e39.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
